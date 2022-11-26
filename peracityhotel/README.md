aca333ad3bf289eaf03c01e91ce0a82d9484e70d
Tahaberk <berksysl.g@gmail.com>
2022-11-26 19:08:52 +0300

    Unnecessary files deleted

--- peracityhotel/README.md
+++
@@ -1,5 +0,0 @@
-# Peracity Hotel Official Web Site
-
-This Web Site Active on : 
-
-<a href="http://peracityhotel.com">peracityhotel.com</a>
--- peracityhotel/mobileMenu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("mobile-menu");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
-  } 
