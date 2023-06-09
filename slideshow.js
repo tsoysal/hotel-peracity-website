2ebff7c88505a15535e31c1fe1a7a4ed5e803ef6
Tahaberk SOYSAL <berksysl.g@gmail.com>
2023-06-09 22:30:12 +0300

    Responsiveness fixed

--- slideshow.js
+++ slideshow.js
@@ -2,23 +2,9 @@ var i = 0;
 var images = [];
 var time = 2000;
 
-images[0] = './assets/slider1.jpg';
-images[1] = './assets/slider2.jpg';
-images[2] = './assets/slider3.jpg';
-images[3] = './assets/slider4.jpg';
-images[4] = './assets/slider5.jpg';
-images[5] = './assets/slider6.jpg';
-images[6] = './assets/slider7.jpg';
-images[7] = './assets/slider8.jpg';
-images[8] = './assets/slider9.jpg';
-images[9] = './assets/slider10.jpg';
-images[10] = './assets/slider11.jpg';
-images[11] = './assets/slider12.jpg';
-images[12] = './assets/slider13.jpg';
-images[13] = './assets/slider14.jpg';
-images[14] = './assets/slider15.jpg';
-images[15] = './assets/slider16.jpg';
-images[16] = './assets/slider17.jpg';
+for(let i = 0; i < 17; i++){
+    images[i] = `./assets/slider${i+1}.jpg`;
+}
 
 function changeImg() {
     document.getElementById('active-image').src = images[i];
